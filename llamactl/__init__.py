a65884c7955b08e6a9f2c281de7c86c163b98a8e
p.jungjitdamrong <p.jungjitdamrong@google.com>
2026-07-27 02:15:07 +0700

    Release v0.3.0

--- llamactl/__init__.py
+++ llamactl/__init__.py
@@ -11,7 +11,7 @@ License: MIT
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2026 Panuwat Jungjitdamrong
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __author__ = "Panuwat Jungjitdamrong"
 __license__ = "MIT"
 __url__ = "https://github.com/p-jungjitdamrong/llama.cpp-controller"
